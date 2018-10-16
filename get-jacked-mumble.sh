0d5530f9935c22f9996f4e22e4e51ea827ff6d1a
Chris Fisher <chris@jupiterbroadcasting.com>
2018-10-16 12:15:04 -0700

    Removed soundboard from Mumble script

--- get-jacked-mumble.sh
+++ get-jacked-mumble.sh
@@ -9,8 +9,8 @@ pacmd unload-module module-jack-sink
 pacmd unload-module module-jack-source
 
 # configure pulse connectors
-pacmd load-module module-jack-source channels=1 source_name=soundboard-in client_name=soundboard-in connect=false
-pacmd load-module module-jack-sink channels=1 sink_name=soundboard-out client_name=soundboard-out connect=false
+#pacmd load-module module-jack-source channels=1 source_name=soundboard-in client_name=soundboard-in connect=false
+#pacmd load-module module-jack-sink channels=1 sink_name=soundboard-out client_name=soundboard-out connect=false
 
 pacmd load-module module-jack-source channels=1 source_name=chrome-in client_name=chrome-in connect=false
 pacmd load-module module-jack-sink channels=1 sink_name=chrome-out client_name=chrome-out connect=false
@@ -21,7 +21,7 @@ pacmd load-module module-jack-source channels=1 source_name=mumble-in client_nam
 pacmd load-module module-jack-sink channels=1 sink_name=mumble-out client_name=mumble-out connect=false
 
 # launch pulse applications
-PULSE_SINK=soundboard-out vlc &
+#PULSE_SINK=soundboard-out vlc &
 PULSE_SINK=chrome-out google-chrome-stable &
 PULSE_SINK=mumble-out PULSE_SOURCE=mumble-in mumble &
 
