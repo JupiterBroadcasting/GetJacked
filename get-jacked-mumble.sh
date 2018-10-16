5d1a9b5ae4206997a915fccf9205a5749c18c064
Chris Fisher <chris@jupiterbroadcasting.com>
2018-10-16 11:54:44 -0700

    Mono updates and sleep increase

--- get-jacked-mumble.sh
+++ get-jacked-mumble.sh
@@ -15,12 +15,15 @@ pacmd load-module module-jack-sink channels=1 sink_name=soundboard-out client_na
 pacmd load-module module-jack-source channels=1 source_name=chrome-in client_name=chrome-in connect=false
 pacmd load-module module-jack-sink channels=1 sink_name=chrome-out client_name=chrome-out connect=false
 
-pacmd load-module module-jack-source channels=1 source_name=spotify-in client_name=spotify-in connect=false
-pacmd load-module module-jack-sink channels=1 sink_name=spotify-out client_name=spotify-out connect=false
+
+# source and sink for mumble
+pacmd load-module module-jack-source channels=1 source_name=mumble-in client_name=mumble-in connect=false
+pacmd load-module module-jack-sink channels=1 sink_name=mumble-out client_name=mumble-out connect=false
 
 # launch pulse applications
 PULSE_SINK=soundboard-out vlc &
 PULSE_SINK=chrome-out google-chrome-stable &
+PULSE_SINK=mumble-out PULSE_SOURCE=mumble-in mumble &
 
 # launch jack applications
-~/bin/REAPER/reaper
+~/bin/REAPER/reaper &
